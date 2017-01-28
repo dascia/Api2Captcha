5e9b5cf8fb531a682c20fa2e012ec9de2fba83a1
dascia <dv.ascencio@gmail.com>
2017-01-28 04:56:24 -0600

    Implement Balance Function
    Balance function retrieves the account balance from 2captcha

---
+++ Api2Captcha/Helper2Captcha.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api2Captcha.Account
+{
+  class Helper2Captcha
+  {
+    /// <summary>
+    /// Gets the account balance
+    /// </summary>
+    /// <param name="apiKey"></param>
+    /// <returns></returns>
+    public static async Task<double> Balance(string apiKey)
+    {
+      string balanceUrl = $"http://2captcha.com/res.php?action=getbalance&key={apiKey}";
+      using (HttpClient client = new HttpClient())
+      {
+        HttpResponseMessage response = await client.GetAsync(balanceUrl);
+        string strResponse = await response.Content.ReadAsStringAsync();
+        double balance;
+        if (double.TryParse(strResponse, out balance) == false) return 0;
+        return balance;
+      }
+    }
+  }
+}
